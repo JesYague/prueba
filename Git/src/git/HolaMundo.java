08e06feade23572a630bed2a928740dd7d258488
JesYague <JesYague@gmail.com>
2022-03-09 13:34:18 +0100

    Nueva versión de mi programa
    Ahora dice mas cosas!
    
    
    Signed-off-by: JesYague <JesYague@gmail.com>

--- Git/src/git/HolaMundo.java
+++ Git/src/git/HolaMundo.java
@@ -3,6 +3,8 @@ package git;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("Que pasa mundo? todo bien? todo correcto?");
+		System.out.println("Y yo que me alegro!!");
 	}
 
 }
